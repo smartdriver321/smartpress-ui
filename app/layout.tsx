1bfb54447a196cbd46f8ba91ad29189faeb2ab48
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-01 21:15:35 +0700

    Sidebar & Command Component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+
 import './globals.css'
 import Navbar from '@/components/Navbar'
+import Sidebar from '@/components/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +21,12 @@ export default function RootLayout({
     <html lang='en'>
       <body className={inter.className}>
         <Navbar />
-        {children}
+        <div className='flex'>
+          <div className='hidden md:block h-[300vh]'>
+            <Sidebar />
+          </div>
+          <div className='p-5 w-full md:max-w-[1140px]'>{children}</div>
+        </div>
       </body>
     </html>
   )
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
+
 import logo from '../img/logo.png'
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
 import {
