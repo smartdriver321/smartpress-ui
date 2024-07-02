44d49cd6ddbbe5820595e2c483a22898abf43be5
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-02 14:21:43 +0700

    Tabs Component

--- components/auth/AuthTabs.tsx
+++ components/auth/AuthTabs.tsx
@@ -1,5 +1,14 @@
-import React from 'react'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 
 export default function AuthTabs() {
-  return <div>AuthTabs</div>
+  return (
+    <Tabs defaultValue='login' className='w-[400px]'>
+      <TabsList className='grid w-full grid-cols-2'>
+        <TabsTrigger value='login'>Login</TabsTrigger>
+        <TabsTrigger value='register'>Register</TabsTrigger>
+      </TabsList>
+      <TabsContent value='login'>Login</TabsContent>
+      <TabsContent value='register'>Register</TabsContent>
+    </Tabs>
+  )
 }
